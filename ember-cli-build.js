188065df507c0445fa763f3964af4ea4f08f7b26
Gabriela <gabilarreglemail@gmail.com>
2024-09-18 19:23:32 +0200

    refact: fix lint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const postcss = require('postcss');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -12,6 +11,7 @@ module.exports = function (defaults) {
           require('tailwindcss')('./tailwind.config.js'),
           require('autoprefixer'),
         ],
+        cacheInclude: [/.*\.(css|hbs|html|js)$/],
       },
     },
   });
