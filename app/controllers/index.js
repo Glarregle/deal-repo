0dedf6aa17c8350298de232dfe45f404fb0e1a09
Gabriela <gabilarreglemail@gmail.com>
2024-09-18 19:02:21 +0200

    chore: add message, style, better form

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,48 +4,57 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 
 export default class IndexController extends Controller {
-	@service router;
+  @service router;
 
 	@tracked orgName = '';
-	@tracked orgMissing = false;
+  @tracked orgName = '';
+  @tracked orgMissing = false;
+  @tracked badKey = false;
+  @tracked error = false;
 
-	@action
-	async handleSubmit() {
-		this._resetErrors();
-		const res = await this._fetchOrganization();
-		console.log({ res }, res.status);
-		if (res.id) {
-			this.router.transitionTo('organization', { name: res.login });
-		} else if (res.status === '404') {
-			this.orgMissing = true;
-		}
-	}
+  @action
+  async handleSubmit(event) {
+    event.preventDefault();
+    this._resetErrors();
+    const res = await this._fetchOrganization();
+    console.log({ res }, res.status);
+    if (res.id) {
+      this.router.transitionTo('organization', { name: res.login });
+    } else if (res.status === '404') {
+      this.orgMissing = true;
+    } else if (res.status === '401') {
+      this.badKey = true;
+    } else {
+      this.error = true;
+    }
+  }
 
-	@action
-	async _fetchOrganization() {
-		const { orgName, token } = this;
-		// try catch
-		if (orgName && token) {
-			const url = `https://api.github.com/orgs/${this.orgName}`;
-			const response = await fetch(url, {
-				headers: {
-					Authorization: `token ${this.token}`,
-				},
-			});
-			return response.json();
-		}
-	}
+  @action
+  async _fetchOrganization() {
+    const { orgName, token } = this;
+    // try catch
+    if (orgName && token) {
+      const url = `https://api.github.com/orgs/${this.orgName}`;
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `token ${this.token}`,
+        },
+      });
+      return response.json();
+    }
+  }
 
-	@action
-	_resetErrors() {
-		this.orgMissing = false;
-	}
+  @action
+  _resetErrors() {
+    this.orgMissing = false;
+    this.badKey = false;
+  }
 
-	@action handleTokenInput(event) {
-		this.token = event.target.value;
-	}
+  @action handleTokenInput(event) {
+    this.token = event.target.value;
+  }
 
-	@action handleNameInput(event) {
-		this.orgName = event.target.value;
-	}
+  @action handleNameInput(event) {
+    this.orgName = event.target.value;
+  }
 }
