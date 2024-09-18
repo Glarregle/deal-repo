7cf67da1938e60da957506fe2bd643c415788910
Gabriela <gabilarreglemail@gmail.com>
2024-09-18 16:33:43 +0200

    chore: install TailwindCSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const postcss = require('postcss');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          require('postcss-import'),
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
   });
 
   return app.toTree();
