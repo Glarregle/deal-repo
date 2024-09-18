242a285b6207a18e1c70a06e849bc2299b56c831
Gabriela <gabilarreglemail@gmail.com>
2024-09-19 01:53:13 +0200

    style: add text-shadow rule to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,16 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.text-shadow': {
+            'text-shadow': '1px 1px 2px rgba(0, 0, 0, 0.7)', // Customize shadow as needed
+          },
+        },
+        ['responsive', 'hover'],
+      );
+    },
+  ],
 };
