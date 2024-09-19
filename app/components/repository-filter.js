046cb5f8cc040c7b350d06579cbf52395aa425ec
Gabriela <gabilarreglemail@gmail.com>
2024-09-20 00:29:15 +0200

    chore: repository-filter contextual component

---
+++ app/components/repository-filter.js
@@ -0,0 +1,82 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class RepoFilterComponent extends Component {
+  @tracked visibility = 'all';
+  @tracked language = 'all';
+
+  get fns() {
+    return {
+      setVisibility: this.setVisibility,
+      setLanguage: this.setLanguage,
+      resetVisibility: this.resetVisibilityFilter,
+      resetLanguage: this.resetLanguageFilter,
+    };
+  }
+
+  get data() {
+    return {
+      filteredRepos: this.filteredRepositories,
+      languageOptions: this.languageOptions,
+      visibilityOptions: this.visibilityOptions,
+      visibility: this.visibility,
+      language: this.language,
+    };
+  }
+
+  get visibilityOptions() {
+    return ['all', 'public', 'private'];
+  }
+
+  get languageOptions() {
+    return this.args.repos?.reduce(
+      (arr, repo) => {
+        const { language } = repo;
+        if (!arr.includes(language)) {
+          arr.push(language);
+        }
+        return arr;
+      },
+      ['all'],
+    );
+  }
+
+  get filteredRepositories() {
+    let repos = [...this.args.repos];
+    if (this._isValue(this.visibility)) {
+      repos = repos.filter((r) =>
+        this.visibility === 'private' ? r.private : !r.private,
+      );
+    }
+    if (this._isValue(this.language))
+      repos = repos.filter((r) => r.language === this.language);
+
+    console.log({ repos });
+    return repos;
+  }
+
+  @action
+  setVisibility(value) {
+    this.visibility = value;
+  }
+
+  @action
+  setLanguage(value) {
+    this.language = value;
+  }
+
+  @action
+  resetVisibilityFilter() {
+    this.visibility = 'all';
+  }
+
+  @action
+  resetLanguageFilter() {
+    this.language = 'all';
+  }
+
+  _isValue(value) {
+    return value !== 'all';
+  }
+}
