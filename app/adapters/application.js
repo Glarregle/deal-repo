a2f4143a0b44eeedc357a0f3c8626a467a4a6fde
Gabriela <gabilarreglemail@gmail.com>
2024-09-19 18:12:46 +0200

    feat: use search api, paginate results and sort

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -9,8 +9,18 @@ export default class ApplicationAdapter extends JSONAPIAdapter {
     this.headers['Authorization'] = `token ${token}`;
   }
 
-  async getRepositories(orgName) {
-    const url = `${this.namespace}/orgs/${orgName}/repos`;
+  async getRepositories(queryString) {
+    const q = encodeURIComponent(queryString)
+    const url = `${this.namespace}/search/repositories?q=${q}&sort=interactions&per_page=30`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: this.headers,
+    });
+    return response.json();
+  }
+
+  async getBranches({ orgName, repo }) {
+    const url = `${this.namespace}/repos/${orgName}/${repo}/branches`;
     const response = await fetch(url, {
       method: 'GET',
       headers: this.headers,
