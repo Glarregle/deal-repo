07521410d007f77792effe2d950e6010de135c0a
Gabriela <gabilarreglemail@gmail.com>
2024-09-20 18:17:26 +0200

    test: repositories provider integration test

---
+++ tests/integration/components/providers/repositories-test.js
@@ -0,0 +1,159 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'tiny-repos/tests/helpers';
+import { render, click, settled, waitFor } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+import { later } from '@ember/runloop';
+
+module('Integration | Component | repositories', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // Mock Search service
+  class MockSearchService extends Service {
+    // Simulate fetching repositories
+    async fetchRepositories(query) {
+      return new Promise((resolve, reject) => {
+        later(() => {
+          if (query.name === 'error') {
+            reject(new Error('Error fetching repositories'));
+          } else {
+            resolve([
+              { id: 1, name: 'Repo 1' },
+              { id: 2, name: 'Repo 2' },
+            ]);
+          }
+        }, 500);
+      });
+    }
+  }
+
+  hooks.beforeEach(function () {
+    this.owner.register('service:search', MockSearchService);
+  });
+
+  test('it can trigger fetch repositories and retrieve results', async function (assert) {
+    this.set('params', { name: 'rails' });
+
+    await render(hbs`<Repositories @params={{this.params}} as |repos|>
+      <div data-test-repositories>
+        {{#if repos.data.hasRepositories}}
+          <ul data-test-repo-list>
+            {{#each repos.data.repositories as |repo|}}
+              <li data-test-repo-item>{{repo.name}}</li>
+            {{/each}}
+          </ul>
+        {{/if}}
+        <button data-test-search-button {{on "click" repos.fns.search}}>Search</button>
+      </div>
+    </Repositories>`);
+
+    // Trigger the search action
+    await click('[data-test-search-button]');
+    // Make sure there are no pending runloops
+    await settled();
+
+    // Verify that repositories are displayed
+    assert
+      .dom('[data-test-repo-item]')
+      .exists({ count: 2 }, 'Two repositories are rendered');
+    assert
+      .dom('[data-test-repo-item]')
+      .includesText('Repo 1', 'First repository is displayed');
+  });
+
+  test('it can handle loading state', async function (assert) {
+    this.set('params', { name: 'test' });
+
+    await render(hbs`<Repositories @params={{this.params}} as |repos|>
+      <div data-test-repositories>
+        <button data-test-search-button {{on "click" repos.fns.search}}>Search</button>
+        
+        {{#if repos.data.hasRepositories}}
+          <ul data-test-repo-list>
+            {{#each repos.data.repositories as |repo|}}
+              <li data-test-repo-item>{{repo.name}}</li>
+            {{/each}}
+          </ul>
+        {{/if}}
+
+        {{#if repos.data.isLoading}}
+          <p data-test-loading>Loading...</p>
+        {{/if}}
+        {{#if repos.data.isError}}
+          <p data-test-error>Error fetching repositories.</p>
+        {{/if}}
+
+        
+      </div>
+    </Repositories>`);
+
+    // Initially, no loading or error message will be shown
+    assert
+      .dom('[data-test-loading]')
+      .doesNotExist('No loading state shown yet');
+
+    // Trigger the search action
+    click('[data-test-search-button]');
+
+    await waitFor('[data-test-loading]');
+
+    assert
+      .dom('[data-test-loading]')
+      .exists('Loading message is shown while promise is pending');
+    // Make sure there are no pending runloops
+    await settled();
+
+    // Verify that the repositories are displayed
+    assert
+      .dom('[data-test-repo-item]')
+      .exists({ count: 2 }, 'Two repositories are rendered');
+    assert
+      .dom('[data-test-repo-item]')
+      .includesText('Repo 1', 'First repository is displayed');
+  });
+
+  test('it can handle error state', async function (assert) {
+    this.set('params', { name: 'error' });
+
+    await render(hbs`<Repositories @params={{this.params}} as |repos|>
+      <div data-test-repositories>
+        {{#if repos.data.isLoading}}
+          <p data-test-loading>Loading...</p>
+        {{/if}}
+        {{#if repos.data.isError}}
+          <p data-test-error>Error fetching repositories.</p>
+        {{/if}}
+         {{#if repos.data.hasRepositories}}
+          <ul data-test-repo-list></ul>
+        {{/if}}
+        <button data-test-search-button {{on "click" repos.fns.search}}>Search</button>
+      </div>
+    </Repositories>`);
+
+    // Initially, no loading or error message will be shown
+    assert
+      .dom('[data-test-loading]')
+      .doesNotExist('No loading state shown yet');
+    assert.dom('[data-test-error]').doesNotExist('No error shown yet');
+    assert.dom('[data-test-repo-list]').doesNotExist('No present repos yet');
+
+    // Trigger the search action
+    click('[data-test-search-button]');
+
+    await waitFor('[data-test-loading]');
+
+    assert
+      .dom('[data-test-loading]')
+      .exists('Loading message is shown while promise is pending');
+
+    // Make sure there are no pending runloops
+    await settled();
+
+    assert
+      .dom('[data-test-error]')
+      .exists('Error message is displayed after failing');
+    assert
+      .dom('[data-test-repo-item]')
+      .doesNotExist('Repo', 'No repositories are rendered');
+  });
+});
