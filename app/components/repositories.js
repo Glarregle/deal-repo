447e97f7abcc89b33494590ba7b5399ffe7b3cbe
Gabriela <gabilarreglemail@gmail.com>
2024-09-20 13:09:06 +0200

    feat: handle repositories error and empty state

--- app/components/repositories.js
+++ app/components/repositories.js
@@ -6,6 +6,7 @@ import { service } from '@ember/service';
 export default class RepositoriesComponent extends Component {
   @service search;
 
+  @tracked isError = false;
   @tracked isLoading = false;
   @tracked repositories = [];
 
@@ -18,14 +19,20 @@ export default class RepositoriesComponent extends Component {
 
   get data() {
     return {
+      isError: this.isError,
       isLoading: this.isLoading,
       repositories: this.results,
       hasLoadMore: this.hasLoadMore,
+      hasRepositories: this.hasRepositories,
     };
   }
 
   get hasLoadMore() {
     return false;
+  }
+
+  get hasRepositories() {
+    return this.results.length > 0;
   }
 
   get results() {
@@ -35,10 +42,14 @@ export default class RepositoriesComponent extends Component {
   @action
   async searchRepos() {
     this.isLoading = true;
+    this.isError = false;
+
     const { name } = this.args.params;
     const query = { name };
     try {
       this.repositories = await this.search.fetchRepositories(query);
+    } catch {
+      this.isError = true;
     } finally {
       this.isLoading = false;
     }
